752caaa0a230e75ae751b51267575e62817cce55
Jonathan Hawk <jonathan@hawk.ninja>
2022-02-27 16:32:50 -0500

    chore: work in progress

--- bin/miles-cdk.js
+++ bin/miles-cdk.js
@@ -3,5 +3,14 @@
 const cdk = require("@aws-cdk/core");
 const { MilesCdkStack } = require("../lib/miles-cdk-stack");
 
+const account = "907147934795";
+const region = "us-east-1";
+
 const app = new cdk.App();
-new MilesCdkStack(app, "MilesCdkStack");
+const envProps = {
+  env: {
+    account: account || process.env.CDK_DEFAULT_ACCOUNT,
+    region: region || process.env.CDK_DEFAULT_REGION,
+  },
+};
+new MilesCdkStack(app, "MilesCdkStack", envProps);
